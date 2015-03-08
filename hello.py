fedf6e0e5bbcd6119586aaf306014fe45b51eb59
bracher2 <bracher2@illinois.edu>
2015-03-08 18:00:31 -0500

    dfvfd

--- hello.py
+++ hello.py
@@ -7,5 +7,20 @@ app = Flask(__name__)
 def hello():
     return 'Hello World!'
 
+@app.route('/color2/<int:color_id>')
+def colorMon(color_id):
+		if color_id == 16449536:
+			return 'red'
+		elif color_id == 65280:
+			return 'green'
+		else:
+			return 'yamon'
+
+@app.route('/color1/<int:red>-<int:green>-<int:blue>')
+def RGB(red, green, blue):
+    return str(red) + ", " + str(green) + ", " + str(blue)
+
 if __name__=='__main__':
 	app.run(debug=True)
+
+	#sdkjjbd
