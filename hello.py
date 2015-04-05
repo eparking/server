081c949891c9d2557ed0a46615b29c7026998520
ssong38 <ssong38@illinois.edu>
2015-04-05 17:56:01 -0500

    Added CRUD

--- hello.py
+++ hello.py
@@ -1,31 +1,39 @@
+#!/venv/bin/python
 import os
-import wingbat 
 import json
 from flask import Flask
 
+print "hello"
 app = Flask(__name__)
 
+class RGBAPixel:
+	def __init__(self,red,green,blue):
+		self.red=red
+		self.green=green
+		self.blue=blue
+	def serialize(self):
+		return {"red":self.red,
+				"green":self.green,
+				"blue":self.blue}
 
-myArray = []
+arr=[]
 
 for i in range(0, 10):
-    myArray.append(wingbat.rgbObject(25*i, 25*i, 25*i))
+    arr.append(RGBAPixel(25*i, 25*i, 25*i))
 
+@app.route('/')
+def hello():
+	return 'Hello World!'
 
 @app.route('/rgb/')
 def printArray():
     counter = 0
     myString = ""
-    for myObject in myArray:
+    for RGBAPixel in arr:
         counter = counter+1
-        print myArray
-        myString = myString + json.dumps(myObject.serialize())
+        print arr
+        myString = myString + json.dumps(RGBAPixel.serialize())
     return myString
-
-
-@app.route('/')
-def hello():
-    return 'Hello World!'
 
 @app.route('/color2/<int:color_id>')
 def colorMon(color_id):
@@ -37,9 +45,58 @@ def colorMon(color_id):
 			return 'yamon'
 
 @app.route('/cooco/<int:red>-<int:green>-<int:blue>')
-def RGB(red, green, blue):
+def cocoo(red, green, blue):
     return str(red) + ", " + str(green) + ", " + str(blue)
+
+
+
+
+
+@app.route('/create/<int:red>-<int:green>-<int:blue>')
+def RGB(red, green,blue):
+	create_obj=RGBAPixel(red,green,blue)
+	arr.append(create_obj)
+	return """<html>
+<body>
+Added color:
+"""+str(red)+", "+str(green)+", "+str(blue)+" at index "+str(len(arr)-1)+"""
+</body>
+</html>
+"""
+
+@app.route('/read/<int:num>')
+def read(num):
+	if (num>=len(arr)):
+			return "invalid index"
+	read_obj=arr[num]
+	return json.dumps(read_obj.serialize())
+
+@app.route('/update/<int:red>-<int:green>-<int:blue>-<int:num>')
+def update(red, green,blue,num):
+	if(num>=len(arr)):
+		return "invalid index"
+	old_obj=arr[num]
+	old_obj.red=red
+	old_obj.green=green
+	old_obj.blue=blue
+	return """<html>
+<body>
+Updated color:
+"""+str(red)+", "+str(green)+", "+str(blue)+" at index "+str(num)+"""
+</body>
+</html>
+"""
+
+
+@app.route('/delete/<int:num>')
+def delete(num):
+	if(num>=len(arr)):
+		return "invalid index"
+	
+	arr.pop(num)
+
+	return "asad"
+
 
 if __name__=='__main__':
 	app.run(debug=True)
-
